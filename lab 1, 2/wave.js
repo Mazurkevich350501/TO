7d443313acd76724a15533f91dc25e8978ce2dd0
evgeny.mazurkevich <evgeny.mazurkevich@kreo.net>
2020-05-01 21:46:10 +0300

    added step counter

--- lab 1, 2/wave.js
+++ lab 1, 2/wave.js
@@ -1,14 +1,22 @@
 export const wave = (graph, startNode, steps, maxCellCount) => {
     let index = 0;
     const cellsToIndex = {};
+    let stepsCount = 0;
+    let ifCount = 0;
+    let ifSuccess = 0;
 
     const makeNewWave = (nodeKey) => {
         cellsToIndex[nodeKey] = index ++;
         const node = graph[nodeKey]
         for (const step of steps) {
+            stepsCount += 1;
             const nextNodeKey = node[step];
+            ifCount += 1;
             if (nextNodeKey && !(nextNodeKey in cellsToIndex)) {
+                ifSuccess += 1;
+                ifCount += 1;
                 if (!maxCellCount || index <= maxCellCount) {
+                    ifSuccess += 1;
                     makeNewWave(nextNodeKey);
                 }
             }
@@ -16,6 +24,12 @@ export const wave = (graph, startNode, steps, maxCellCount) => {
     } 
 
     makeNewWave(startNode);
+
+    console.log(
+        'steps:', stepsCount,
+        '\nif count:', ifCount,
+        '\nif (true):', ifSuccess,
+    )
     
     return Object.entries(cellsToIndex)
         .sort(([_a, indexA], [_b, indexB]) => indexA - indexB)
